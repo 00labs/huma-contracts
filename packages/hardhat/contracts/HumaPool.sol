1620b3e181012f45216854d4c18de664e0293f8e
Michael Liu <michael.liu95@gmail.com>
2022-06-21 16:39:46 -0400

    Revert "Revert "Pool tranches""
    This reverts commit 004a73bf393cf2475ed253ec77766714c400485e.

--- packages/hardhat/contracts/HumaPool.sol
+++ packages/hardhat/contracts/HumaPool.sol
@@ -4,28 +4,62 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HumaPool is Ownable {
+  IERC20 public immutable poolToken;
+  uint256 private immutable poolTokenDecimals;
+
+  struct PoolTranche {
+    uint256 humaScoreLowerBound;
+    uint256 interestRate;
+    uint256 collateralRequired;
+  }
+  PoolTranche[] public tranches;
+
   enum PoolStatus {
-    on,
-    off
+    open,
+    closed
+  }
+  PoolStatus public status = PoolStatus.open;
+
+  constructor(address _poolToken) payable {
+    poolToken = IERC20(_poolToken);
+    poolTokenDecimals = ERC20(_poolToken).decimals();
   }
 
-  PoolStatus public status = PoolStatus.off;
+  modifier poolOpen() {
+    require(
+      status == PoolStatus.open,
+      "HumaPool: Pool is not open. The owner must call enablePool"
+    );
+  }
 
-  constructor() payable {}
+  function getPoolTranches() external view returns (PoolTranche[] memory) {
+    return tranches;
+  }
+
+  function setPoolTranches(PoolTranche[] calldata _tranches) external {
+    uint256 lastHumaScore = 100;
+    for (uint256 i = 0; i < _tranches.length; i++) {
+      require(_tranches[i].humaScoreLowerBound < lastHumaScore);
+      require(_tranches[i].interestRate >= 0);
+      require(_tranches[i].collateralRequired >= 0);
+    }
+
+    tranches = _tranches;
+  }
 
   // Allow borrow applications and loans to be processed by this pool.
   function enablePool() external onlyOwner {
-    status = PoolStatus.on;
+    status = PoolStatus.open;
   }
 
   // Reject all future borrow applications and loans. Note that existing
   // loans will still be processed as expected.
   function disablePool() external onlyOwner {
-    status = PoolStatus.off;
+    status = PoolStatus.closed;
   }
 
   // Function to receive Ether. msg.data must be empty
