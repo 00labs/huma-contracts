cecd093302ae1058428ee79ee041b4b3eca1e6f3
Michael Liu <michael.liu95@gmail.com>
2022-06-21 16:35:45 -0400

    Revert "Extracting admins to separate file"
    This reverts commit 4eab74c2b36148d6cb4de723e1e9d6afcdfb9e7c.

--- packages/hardhat/contracts/HumaPoolFactory.sol
+++ packages/hardhat/contracts/HumaPoolFactory.sol
@@ -5,12 +5,14 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./HumaPool.sol";
-import "./HumaAdmins.sol";
 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract HumaPoolFactory {
-  HumaAdmins humaAdmins;
+contract HumaPoolFactory is Ownable {
+  // A map of addresses which are allowed to create new Huma Pools.
+  // Key: address
+  // Value: Whether this address is currently allowed to create a pool
+  mapping(address => bool) public approvedAdmins;
 
   // Array of all Huma Pools created from this factory
   address[] public pools;
@@ -24,17 +26,33 @@ contract HumaPoolFactory {
     // what should we do on deploy?
   }
 
+  // Add a new admin to the approved admins list. By default they
+  // won't be allowed to create pools right away. The owner must
+  // call `enableApprovedPoolAdmin` to give the admin creation privileges
+  function addApprovedPoolAdmin(address _admin) external onlyOwner {
+    approvedAdmins[_admin] = false;
+  }
+
+  // Disable an admin from being able to create new huma pools
+  function disableApprovedPoolAdmin(address _admin) external onlyOwner {
+    approvedAdmins[_admin] = false;
+  }
+
+  // Grant an admin Huma Pool creation privileges
+  function enableApprovedPoolAdmin(address _admin) external onlyOwner {
+    approvedAdmins[_admin] = true;
+  }
+
   function setMinimumLiquidityNeededUSD(uint256 _minimumLiquidityNeededUSD)
     external
+    onlyOwner
   {
-    humaAdmins.isMasterAdmin();
     minimumLiquidityNeededUSD = _minimumLiquidityNeededUSD;
   }
 
   function deployNewPool() external payable returns (address humaPool) {
     require(msg.value >= minimumLiquidityNeededUSD);
-    humaAdmins.isApprovedAdmin();
-
+    require(approvedAdmins[msg.sender] == true);
     humaPool = address(new HumaPool());
     pools.push(humaPool);
 
