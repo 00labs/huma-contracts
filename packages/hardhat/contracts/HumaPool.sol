ea892ea50a6509089b5ce2e0584e8d07606cc0bc
Michael Liu <michael.liu95@gmail.com>
2022-06-21 16:39:48 -0400

    Revert "Revert "Withdraw and deposit logic""
    This reverts commit c15d9d5d6937a3adb70ca75d2db85dbaf1468842.

--- packages/hardhat/contracts/HumaPool.sol
+++ packages/hardhat/contracts/HumaPool.sol
@@ -4,12 +4,20 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "./HumaPoolSafeFactory.sol";
+
 contract HumaPool is Ownable {
+  using SafeERC20 for IERC20;
+
   IERC20 public immutable poolToken;
   uint256 private immutable poolTokenDecimals;
+
+  HumaPoolSafe private poolSafe;
+
+  mapping(address => uint256) liquidityMapping;
 
   struct PoolTranche {
     uint256 humaScoreLowerBound;
@@ -19,28 +27,31 @@ contract HumaPool is Ownable {
   PoolTranche[] public tranches;
 
   enum PoolStatus {
-    open,
-    closed
+    on,
+    off
   }
-  PoolStatus public status = PoolStatus.open;
+  PoolStatus public status = PoolStatus.on;
 
   constructor(address _poolToken) payable {
     poolToken = IERC20(_poolToken);
     poolTokenDecimals = ERC20(_poolToken).decimals();
+    poolSafe = HumaPoolSafeFactory.deployNewPoolSafe(address(this), _poolToken);
   }
 
-  modifier poolOpen() {
-    require(
-      status == PoolStatus.open,
-      "HumaPool: Pool is not open. The owner must call enablePool"
-    );
+  modifier poolOn() {
+    require(status == PoolStatus.on, "HumaPool:POOL_NOT_ON");
+    _;
   }
 
   function getPoolTranches() external view returns (PoolTranche[] memory) {
     return tranches;
+    _;
   }
 
-  function setPoolTranches(PoolTranche[] calldata _tranches) external {
+  function setPoolTranches(PoolTranche[] calldata _tranches)
+    external
+    onlyOwner
+  {
     uint256 lastHumaScore = 100;
     for (uint256 i = 0; i < _tranches.length; i++) {
       require(_tranches[i].humaScoreLowerBound < lastHumaScore);
@@ -51,15 +62,26 @@ contract HumaPool is Ownable {
     tranches = _tranches;
   }
 
+  function deposit(uint256 liquidityAmount) external poolOn {
+    poolToken.safeTransferFrom(msg.sender, poolSafe, liquidityAmount);
+    liquidityMapping[msg.sender] += liquidityAmount;
+  }
+
+  function withdraw(uint256 amount) external {
+    require(amount <= liquidityMapping[msg.sender]);
+    liquidityMapping[msg.sender] -= amount;
+    poolSafe.transfer(msg.sender, amount);
+  }
+
   // Allow borrow applications and loans to be processed by this pool.
   function enablePool() external onlyOwner {
-    status = PoolStatus.open;
+    status = PoolStatus.on;
   }
 
   // Reject all future borrow applications and loans. Note that existing
   // loans will still be processed as expected.
   function disablePool() external onlyOwner {
-    status = PoolStatus.closed;
+    status = PoolStatus.off;
   }
 
   // Function to receive Ether. msg.data must be empty
