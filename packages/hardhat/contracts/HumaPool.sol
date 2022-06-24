f24f07a1f377cf5ab96993256ca165323709314a
Michael Liu <michael.liu95@gmail.com>
2022-06-24 17:48:52 -0400

    Add modifier onlyOwnerOrHumaMasterAdmin
    Previously, only the owner of the pool would have been allowed to call certain sensitive functions. Opening up those permissions to also allow the huma master admin to change pool settings.

--- packages/hardhat/contracts/HumaPool.sol
+++ packages/hardhat/contracts/HumaPool.sol
@@ -7,12 +7,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "./interfaces/IHumaPoolAdmins.sol";
 import "./interfaces/IHumaPoolLoanHelper.sol";
 import "./interfaces/IHumaPoolLockerFactory.sol";
 import "./interfaces/IHumaPoolLocker.sol";
 
 contract HumaPool is Ownable {
   using SafeERC20 for IERC20;
+
+  // HumaPoolAdmins
+  address private immutable humaPoolAdmins;
 
   IERC20 public immutable poolToken;
   uint256 private immutable poolTokenDecimals;
@@ -47,7 +51,7 @@ contract HumaPool is Ownable {
     uint256 interestRate;
     uint256 collateralRequired;
   }
-  PoolTranche[] public tranches;
+  PoolTranche[] private tranches;
 
   enum PoolStatus {
     On,
@@ -59,13 +63,29 @@ contract HumaPool is Ownable {
   // before they can withdraw their capital
   uint256 loanWithdrawalLockoutPeriod = 2630000;
 
-  constructor(address _poolToken, address _poolLockerFactory) {
+  constructor(
+    address _poolToken,
+    address _poolLockerFactory,
+    address _humaPoolAdmins
+  ) {
     poolToken = IERC20(_poolToken);
     poolTokenDecimals = ERC20(_poolToken).decimals();
     poolLocker = IHumaPoolLockerFactory(_poolLockerFactory).deployNewPoolLocker(
         address(this),
         _poolToken
       );
+    humaPoolAdmins = _humaPoolAdmins;
+  }
+
+  // Allow for sensitive pool functions only to be called by
+  // the pool owner and the huma master admin
+  modifier onlyOwnerOrHumaMasterAdmin() {
+    require(
+      (msg.sender == owner() ||
+        IHumaPoolAdmins(humaPoolAdmins).isMasterAdmin() == true),
+      "HumaPool:PERMISSION_DENIED_NOT_ADMIN"
+    );
+    _;
   }
 
   // In order for a pool to issue new loans, it must be turned on by an admin
@@ -87,7 +107,10 @@ contract HumaPool is Ownable {
   // The tranches must be passed in descending order and define the loan
   // strategy for the risk tranche from humaScoreLowerBound <-> next tranche bound (or MAX_INT) inclusive
   // A higher Huma score = less risky loan
-  function setPoolTranches(PoolTranche[] memory _tranches) external onlyOwner {
+  function setPoolTranches(PoolTranche[] memory _tranches)
+    external
+    onlyOwnerOrHumaMasterAdmin
+  {
     uint256 lastHumaScore = 2**256 - 1; // MAX_INT
     delete tranches;
     for (uint256 i = 0; i < _tranches.length; i++) {
@@ -113,7 +136,7 @@ contract HumaPool is Ownable {
 
   function setHumaPoolLoanHelper(address _humaPoolLoanHelper)
     external
-    onlyOwner
+    onlyOwnerOrHumaMasterAdmin
   {
     humaPoolLoanHelper = _humaPoolLoanHelper;
     // New loan helpers must be reviewed and approved by the Huma team.
@@ -123,14 +146,14 @@ contract HumaPool is Ownable {
   // TODO: Add function to approve pool loan helper (only callable by huma)
 
   // Allow borrow applications and loans to be processed by this pool.
-  function enablePool() external onlyOwner {
+  function enablePool() external onlyOwnerOrHumaMasterAdmin {
     require(tranches.length > 0);
     status = PoolStatus.On;
   }
 
   // Reject all future borrow applications and loans. Note that existing
   // loans will still be processed as expected.
-  function disablePool() external onlyOwner {
+  function disablePool() external onlyOwnerOrHumaMasterAdmin {
     status = PoolStatus.Off;
   }
 
@@ -140,7 +163,7 @@ contract HumaPool is Ownable {
 
   function setLoanWithdrawalLockoutPeriod(uint256 _loanWithdrawalLockoutPeriod)
     external
-    onlyOwner
+    onlyOwnerOrHumaMasterAdmin
   {
     loanWithdrawalLockoutPeriod = _loanWithdrawalLockoutPeriod;
   }
