caeabbb369a90e89d766f918fb4ede2762e8ff7c
Richard Liu <richard@huma.finance>
2022-06-27 01:18:33 -0700

    Reworked on SBT. Abandoned previous ERC20-based implementation. Introduced ERC721-based implementation

---
+++ packages/hardhat/contracts/HumaCreditTracking.sol
@@ -0,0 +1,177 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interfaces/ICreditTracking.sol";
+import "./HumaCreditTrackingToken.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import {Base64} from "./libraries/Base64.sol";
+import "hardhat/console.sol";
+
+/**
+ * @notice Huma Credit Tracking is a credit reporting service that uses HumaCreditTrackingToken,
+ * a non-transferrable NFT to track wallet's new borrowings, payoffs, and defaults.
+ *
+ */
+contract HumaCreditTracking is ICreditTracking {
+    address owner;
+
+    struct CreditTrackingRecord {
+        uint256 tokenId;
+        uint256 numOfPayoffs;
+        uint256 numOfDefaults;
+        uint256 numOfOutstandingLoans;
+    }
+
+    // Mapping owner address to credit tracking record.
+    mapping(address => CreditTrackingRecord) private _records;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    HumaCreditTrackingToken private ctt;
+
+    constructor(address _owner) {
+        owner = _owner;
+        ctt = new HumaCreditTrackingToken(
+            "Huma Credit Tracking Token",
+            "HumaCTT"
+        );
+    }
+
+    /**
+     * @notice Tracks a borrowing initiated for the wallet owner
+     */
+    function reportBorrowing(address borrower) external virtual override {
+        require(
+            borrower == msg.sender || owner == msg.sender,
+            "Only the borrower or the contract owner can request a credit tracking."
+        );
+
+        if (0 == ctt.balanceOf(borrower)) {
+            // Brand-new user, mint the NFT to the user, and set initial values to the counters
+            uint256 newItemId = _tokenIds.current();
+            ctt.safeMint(borrower, newItemId);
+            _tokenIds.increment();
+            _records[borrower] = CreditTrackingRecord(newItemId, 0, 0, 1);
+        } else {
+            _records[borrower].numOfOutstandingLoans += 1;
+        }
+
+        // update URI.
+        string memory newTokenUri = _getURI(borrower, _records[borrower]);
+        ctt.setTokenURI(_records[borrower].tokenId, newTokenUri);
+
+        emit NewCreditTracking(borrower, owner, newTokenUri);
+    }
+
+    /**
+     * @notice Tracks a pay off made by the borrower
+     */
+    function reportPayoff(address borrower) external virtual override {
+        require(
+            owner == msg.sender,
+            "Only the contract owner can report a payoff."
+        );
+
+        require(
+            _records[msg.sender].numOfOutstandingLoans > 0,
+            "There is no outstanding loan record for the borrower."
+        );
+
+        _records[borrower].numOfPayoffs += 1;
+        _records[borrower].numOfOutstandingLoans -= 1;
+
+        // update URI
+        string memory newTokenUri = _getURI(borrower, _records[borrower]);
+        ctt.setTokenURI(_records[borrower].tokenId, newTokenUri);
+
+        emit CreditPaidoff(borrower, owner, newTokenUri);
+    }
+
+    /**
+     * @notice Tracks a default of a loan for the borrower
+     */
+    function reportDefault(address borrower) external virtual override {
+        require(
+            owner == msg.sender,
+            "Only the contract owner can report a payoff."
+        );
+
+        // Question: Shall we go ahead to record default even if something
+        // is wrong with the outstandingLoans counter in our system.
+        require(
+            _records[msg.sender].numOfOutstandingLoans > 0,
+            "There is no tracking record for the borrower."
+        );
+
+        _records[msg.sender].numOfDefaults += 1;
+        _records[msg.sender].numOfOutstandingLoans -= 1;
+
+        // update URI
+        string memory newTokenUri = _getURI(borrower, _records[borrower]);
+        ctt.setTokenURI(_records[borrower].tokenId, newTokenUri);
+
+        emit CreditDefault(borrower, owner, newTokenUri);
+    }
+
+    /**
+     * @notice Allows the contract to remove the tracking of a borrower.
+     * This will highly likely by the borrower's request, and we will leave that
+     * to the application to manage it.
+     */
+    function revokeTracking(address borrower) external virtual override {
+        require(
+            owner == msg.sender,
+            "Only the contract owner can report a payoff."
+        );
+
+        ctt.burn(_records[borrower].tokenId);
+
+        emit CreditTrackingRevoked(msg.sender, owner);
+    }
+
+    function _getURI(address borrower, CreditTrackingRecord storage ctr)
+        private
+        view
+        returns (string memory)
+    {
+        string
+            memory ipfs_loc = "https://ipfs.io/ipfs/Qmf23jkGtxXwi4u4MiMmnKLA98rkGXnF19nWfLD1S7GXJt";
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        "{",
+                        '"name": "HumaCTT-',
+                        Strings.toHexString(uint160(borrower), 20),
+                        '", ',
+                        '"description": "Huma Credit Tracking Token.", ',
+                        '"image": "',
+                        ipfs_loc,
+                        '", ',
+                        '"attributes": [ ',
+                        '{"trait_type": "# of payoffs", "value": "',
+                        ctr.numOfPayoffs,
+                        '"}, ',
+                        '{"trait_type": "# of defaults", "value": "',
+                        ctr.numOfDefaults,
+                        '"},',
+                        '{"trait_type": "# of outstanding loans", "value": "',
+                        ctr.numOfOutstandingLoans,
+                        '"}',
+                        "]}"
+                    )
+                )
+            )
+        );
+
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return finalTokenUri;
+    }
+}
---
+++ packages/hardhat/contracts/HumaCreditTrackingToken.sol
@@ -0,0 +1,120 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "hardhat/console.sol";
+
+/**
+ * @notice HumaCTT (Huma Credit Tracking Token) is a non-transferrable token
+ * that is issued to the borrower to capture people's borrowing and payback record.
+ * It is a SBT. The user cannot transfer away the token. If they do not want the
+ * token, they can only request the contract to revoke it if there is no outstanding balance.
+ *
+ * The CTT is impelmented as an NFT that can be viewed on Opensea.
+ * Each wallet can have up to one CTT. In the metadata, it is currently tracking
+ * # of successful payoffs, # of defaults, # of outstanding loans.
+ * The list of attributes to be tracked can grow over time.
+ *
+ */
+contract HumaCreditTrackingToken is ERC721URIStorage {
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    /**
+     * @notice Overrides the default behavior. The token is NOT transferred away
+     * from the "from" address to achieve "non-transferrable" effect.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        /// @dev Intentionally do nothing to make the token non-transferrable
+    }
+
+    /**
+     * @notice Overrides the default behavior. The token is NOT transferred away
+     * from the "from" address to achieve "non-transferrable" effect.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) public override {
+        /// @dev Intentionally do nothing to make the token non-transferrable
+    }
+
+    /**
+     * @notice Overrides the default behavior. The token is NOT transferred away
+     * from the "from" address to achieve "non-transferrable" effect.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        /// @dev Intentionally do nothing to make the token non-transferrable
+    }
+
+    /**
+     * @notice Overrides the default behavior. The token is NOT transferred away
+     * from the "from" address to achieve "non-transferrable" effect.
+     */
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) internal override {
+        /// @dev Intentionally do nothing w/ the request to make the token non-transferrable
+    }
+
+    /**
+     * @notice Overrides the default behavior. The token is NOT transferred away
+     * from the "from" address to achieve "non-transferrable" effect.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        /// @dev Intentionally do nothing w/ the request to make the token non-transferrable
+    }
+
+    /**
+     * @dev Safely mints `tokenId` and transfers it to `to`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeMint(address to, uint256 tokenId) external {
+        _safeMint(to, tokenId, "");
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) external {
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    /**
+     * @dev See {ERC721-_burn}. This override additionally checks to see if a
+     * token-specific URI was set for the token, and if so, it deletes the token URI from
+     * the storage mapping.
+     */
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
+    }
+}
