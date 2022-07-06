9f92b9adf3193df54c94737f24cb532610dc3343
Richard Liu <richard@huma.finance>
2022-07-06 09:57:35 -0700

    Updated FDT, introduced FDTWithLosses to handle losses

--- packages/hardhat/contracts/FDT/FDT.sol
+++ packages/hardhat/contracts/FDT/FDT.sol
@@ -2,81 +2,118 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./interfaces/IFundsDistributionToken.sol";
+import "./interfaces/IFDT.sol";
 import "../libraries/SafeMathInt.sol";
 import "../libraries/SafeMathUint.sol";
 
 /**
- * @dev A  mintable token that can represent claims on cash flow of arbitrary assets such as dividends, loan repayments,
+ * @title Funds Distribution Token
+ * @notice A mintable token that can represent claims on cash flow of arbitrary assets such as dividends, loan repayments,
  * fee or revenue shares among large numbers of token holders. Anyone can deposit funds, token holders can withdraw
  * their claims.
- * FundsDistributionToken (FDT) implements the accounting logic. FDT-Extension contracts implement methods for depositing and
- * withdrawing funds in Ether or according to a token standard such as ERC20, ERC223, ERC777.
  *
  * Code referenced https://github.com/atpar/funds-distribution-token/blob/master/contracts/FundsDistributionToken.sol
  */
-abstract contract FundsDistributionToken is IFundsDistributionToken, ERC20 {
+abstract contract FDT is IFDT, ERC20 {
     using SafeMath for uint256;
     using SafeMathInt for int256;
     using SafeMathUint for uint256;
+    using SafeERC20 for IERC20;
 
     // optimize, see https://github.com/ethereum/EIPs/issues/1726#issuecomment-472352728
     uint256 internal constant pointsMultiplier = 2**128;
+
+    /// Tracks the earning per share
     uint256 internal pointsPerShare;
 
+    /**
+     * Accumulative earning adjustment per account.
+     * pointsCorrection is used when tokens are minted to the owner on different dates
+     * or transferred between accounts.
+     */
     mapping(address => int256) internal pointsCorrection;
+
+    /// Amount that has withdrawn by the account owner
     mapping(address => uint256) internal withdrawnFunds;
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    /// // The underlying token that the FDT owners can claim interest for
+    IERC20 public immutable fundsToken;
+
+    /// total amount of the interest for the FDT that has not been withdrawn
+    uint256 public fundsBalance;
 
     /**
-     * prev. distributeDividends
+     * @param name the name of the token
+     * @param symbol the symbol of the token
+     * @param _fundsToken the asset token that leads to the FDT's gain and losses
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        address _fundsToken
+    ) ERC20(name, symbol) {
+        fundsToken = IERC20(_fundsToken);
+    }
+
+    /**
      * @notice Distributes funds to token holders.
      * @dev It reverts if the total supply of tokens is 0.
-     * It emits the `FundsDistributed` event if the amount of received ether is greater than 0.
+     * It emits the `FundsDistributed` event if the amount of received is greater than 0.
      * About undistributed funds:
      *   In each distribution, there is a small amount of funds which does not get distributed,
      *     which is `(msg.value * pointsMultiplier) % totalSupply()`.
      *   With a well-chosen `pointsMultiplier`, the amount funds that are not getting distributed
      *     in a distribution can be less than 1 (base unit).
-     *   We can actually keep track of the undistributed ether in a distribution
+     *   We can actually keep track of the undistributed in a distribution
      *     and try to distribute it in the next distribution ....... todo implement
      */
-    function _distributeFunds(uint256 value) internal {
-        require(
-            totalSupply() > 0,
-            "FundsDistributionToken._distributeFunds: SUPPLY_IS_ZERO"
-        );
+    function distributeFunds(uint256 value) public virtual override {
+        require(totalSupply() > 0, "FDT._distributeFunds: SUPPLY_IS_ZERO");
 
         if (value > 0) {
             pointsPerShare = pointsPerShare.add(
                 value.mul(pointsMultiplier) / totalSupply()
             );
+
             emit FundsDistributed(msg.sender, value);
         }
     }
 
     /**
-     * prev. withdrawDividend
-     * @notice Prepares funds withdrawal
-     * @dev It emits a `FundsWithdrawn` event if the amount of withdrawn ether is greater than 0.
+     * @dev Withdraws all available funds for a token holder.
      */
-    function _prepareWithdraw() internal returns (uint256) {
-        uint256 _withdrawableDividend = withdrawableFundsOf(msg.sender);
+    function withdrawFunds() external virtual override {
+        uint256 _withdrawableFund = withdrawableFundsOf(msg.sender);
 
         withdrawnFunds[msg.sender] = withdrawnFunds[msg.sender].add(
-            _withdrawableDividend
+            _withdrawableFund
         );
 
-        emit FundsWithdrawn(msg.sender, _withdrawableDividend);
+        if (_withdrawableFund > uint256(0)) {
+            fundsToken.safeTransfer(msg.sender, _withdrawableFund);
 
-        return _withdrawableDividend;
+            emit FundsWithdrawn(msg.sender, _withdrawableFund);
+        }
     }
 
     /**
-     * prev. withdrawableDividendOf
-     * @notice View the amount of funds that an address can withdraw.
+     * @dev Since withdrawFunds() does not return per EIP-2222, this is a hack to allow
+     * the client know the amount that has been withdrawn. The calling sequence is:
+     *     account.withdrawFunds();
+     *     int amount = _getFundsBalanceChanges();
+     */
+    function _getFundsBalanceChanges() internal virtual returns (int256) {
+        uint256 _prevFundsBalance = fundsBalance;
+
+        fundsBalance = fundsToken.balanceOf(address(this));
+
+        return int256(fundsBalance).sub(int256(_prevFundsBalance));
+    }
+
+    /**
+     * @notice Views the amount of funds that an address can withdraw.
      * @param _owner The address of a token holder.
      * @return The amount funds that `_owner` can withdraw.
      */
@@ -85,13 +122,7 @@ abstract contract FundsDistributionToken is IFundsDistributionToken, ERC20 {
     }
 
     /**
-     * @dev Withdraws all available funds for a FundsDistributionToken holder.
-     */
-    function withdrawFunds() external virtual override {}
-
-    /**
-     * prev. withdrawnDividendOf
-     * @notice View the amount of funds that an address has withdrawn.
+     * @notice Views the amount of funds that an address has withdrawn.
      * @param _owner The address of a token holder.
      * @return The amount of funds that `_owner` has withdrawn.
      */
@@ -100,8 +131,7 @@ abstract contract FundsDistributionToken is IFundsDistributionToken, ERC20 {
     }
 
     /**
-     * prev. accumulativeDividendOf
-     * @notice View the amount of funds that an address has earned in total.
+     * @notice Views the amount of funds that an address has earned in total.
      * @dev accumulativeFundsOf(_owner) = withdrawableFundsOf(_owner) + withdrawnFundsOf(_owner)
      * = (pointsPerShare * balanceOf(_owner) + pointsCorrection[_owner]) / pointsMultiplier
      * @param _owner The address of a token holder.
@@ -116,6 +146,9 @@ abstract contract FundsDistributionToken is IFundsDistributionToken, ERC20 {
                 .toUint256Safe() / pointsMultiplier;
     }
 
+    // *****************************
+    // * IERC20 Functions          *
+    // *****************************
     /**
      * @dev Internal function that transfer tokens from one address to another.
      * Update pointsCorrection to keep funds unchanged.
@@ -127,7 +160,7 @@ abstract contract FundsDistributionToken is IFundsDistributionToken, ERC20 {
         address from,
         address to,
         uint256 value
-    ) internal override {
+    ) internal virtual override {
         super._transfer(from, to, value);
 
         int256 _magCorrection = pointsPerShare.mul(value).toInt256Safe();
---
+++ packages/hardhat/contracts/FDT/FDTWithLosses.sol
@@ -0,0 +1,191 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./FDT.sol";
+import "./interfaces/IFDTWithLosses.sol";
+import "../libraries/SafeMathInt.sol";
+import "../libraries/SafeMathUint.sol";
+
+/**
+ * @title FDT that supports losses
+ * @notice FDTWithLosses allows an FDT to distribute and recognize losses.
+ */
+abstract contract FDTWithLosses is FDT, IFDTWithLosses {
+    using SafeMath for uint256;
+    using SafeMathInt for int256;
+    using SafeMathUint for uint256;
+
+    /// Sum of recognized losses. Not sure if we really need it. todo verify it
+    uint256 public totalRecognizedLosses;
+
+    /// reported losses per share
+    uint256 internal lossesPerShare;
+
+    /**
+     * Accumulative losses adjustment per account.
+     * lossesCorrection is used when tokens are minted to the owner on different dates
+     * or transferred between accounts.
+     */
+    mapping(address => int256) internal lossesCorrection;
+
+    /// Recognized losses per account
+    mapping(address => uint256) internal recognizedLosses;
+
+    /**
+     * @param name the name of the token
+     * @param symbol the symbol of the token
+     * @param _fundsToken the asset token that leads to the FDT's gain and losses
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        address _fundsToken
+    ) FDT(name, symbol, _fundsToken) {}
+
+    /**
+     * @notice Distributes losses to FDT token holders.
+     * @dev It reverts if the total supply of tokens is 0.
+     * It emits the `LossesDistributed` event if the amount of received is greater than 0.
+     * About undistributed losses:
+     *   In each distribution, there is a small amount of funds which does not get distributed,
+     *     which is `(msg.value * pointsMultiplier) % totalSupply()`.
+     *   With a well-chosen `pointsMultiplier`, the amount funds that are not getting distributed
+     *     in a distribution can be less than 1 (base unit).
+     *   We can actually keep track of the undistributed in a distribution
+     *     and try to distribute it in the next distribution ....... todo implement
+     */
+    function distributeLosses(uint256 amount) public virtual override {
+        require(
+            totalSupply() > 0,
+            "FDTWithLosses.distributeLosses:ZERO_SUPPLY"
+        );
+
+        if (amount > 0) {
+            lossesPerShare = lossesPerShare.add(
+                amount.mul(pointsMultiplier) / totalSupply()
+            );
+            emit LossesDistributed(msg.sender, amount);
+        }
+    }
+
+    /**
+     * @notice Withdraws all available losses for a token holder.
+     */
+    function recognizeLosses() external virtual override returns (uint256) {
+        uint256 _recognizableLosses = recognizableLossesOf(msg.sender);
+
+        recognizedLosses[msg.sender] = recognizedLosses[msg.sender].add(
+            _recognizableLosses
+        );
+
+        emit LossesRecognized(msg.sender, _recognizableLosses);
+
+        return _recognizableLosses;
+    }
+
+    /**
+     * @notice Views the losses that an address can recognize.
+     * @param _owner The address of a token holder.
+     * @return The amount of losses to be recognized for the `_owner`.
+     */
+    function recognizableLossesOf(address _owner)
+        public
+        view
+        returns (uint256)
+    {
+        return accumulativeLossesOf(_owner).sub(recognizedLosses[_owner]);
+    }
+
+    /**
+     * @notice Views the losses that an address has recognized.
+     * @param _owner The address of a token holder.
+     * @return The amount of losses recognized for the `_owner`.
+     */
+    function recognizedLossesOf(address _owner) public view returns (uint256) {
+        return recognizedLosses[_owner];
+    }
+
+    /**
+     * @notice Views the amount of losses that an address has accumulated in total.
+     * @dev accumulativeLossesOf(_owner) = recognizableLossesOf(_owner) + recognizedLossesOf(_owner)
+     * = (lossesPerShare * balanceOf(_owner) + lossesCorrection[_owner]) / pointsMultiplier
+     * @param _owner The address of a token holder.
+     * @return The amount of losses that `_owner` has ammulumated in total.
+     */
+    function accumulativeLossesOf(address _owner)
+        public
+        view
+        returns (uint256)
+    {
+        return
+            lossesPerShare
+                .mul(balanceOf(_owner))
+                .toInt256Safe()
+                .add(lossesCorrection[_owner])
+                .toUint256Safe() / pointsMultiplier;
+    }
+
+    // *****************************
+    // * IERC20 Functions          *
+    // *****************************
+    /**
+     * @dev Internal function that transfer tokens from one address to another.
+     * Update pointsCorrection and lossCorrection to keep funds unchanged.
+     * @param from The address to transfer from.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 value
+    ) internal override {
+        super._transfer(from, to, value);
+
+        int256 _Correction = pointsPerShare.mul(value).toInt256Safe();
+        pointsCorrection[from] = pointsCorrection[from].add(_Correction);
+        pointsCorrection[to] = pointsCorrection[to].sub(_Correction);
+
+        int256 _lossCorrection = lossesPerShare.mul(value).toInt256Safe();
+        lossesCorrection[from] = lossesCorrection[from].add(_lossCorrection);
+        lossesCorrection[to] = lossesCorrection[to].sub(_lossCorrection);
+    }
+
+    /**
+     * @dev Internal function that mints tokens to an account.
+     * Update pointsCorrection and lossCorrection to keep funds unchanged.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal virtual override {
+        super._mint(account, value);
+
+        pointsCorrection[account] = pointsCorrection[account].sub(
+            (pointsPerShare.mul(value)).toInt256Safe()
+        );
+
+        lossesCorrection[account] = lossesCorrection[account].sub(
+            (lossesPerShare.mul(value)).toInt256Safe()
+        );
+    }
+
+    /**
+     * @dev Internal function that burns an amount of the token of a given account.
+     * Update pointsCorrection and lossCorrection to keep funds unchanged.
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function _burn(address account, uint256 value) internal virtual override {
+        super._burn(account, value);
+
+        pointsCorrection[account] = pointsCorrection[account].add(
+            (pointsPerShare.mul(value)).toInt256Safe()
+        );
+
+        lossesCorrection[account] = lossesCorrection[account].add(
+            (lossesPerShare.mul(value)).toInt256Safe()
+        );
+    }
+}
