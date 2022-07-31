450c3f44361d41f9c87ef127b2a9e59cdfdd3270
wisdant <richard.y.liu@gmail.com>
2022-07-30 19:14:23 -0700

    add todo comment

--- packages/hardhat/contracts/HumaPoolAdmins.sol
+++ packages/hardhat/contracts/HumaPoolAdmins.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./HumaPool.sol";
 
+// todo Combine this into HumaConfig, streamline key roles: SuperAdmin, Pausers, PoolAdmins.
 contract HumaPoolAdmins is Ownable {
     // A map of addresses which are allowed to create new Huma Pools.
     mapping(address => bool) private approvedAdmins;
