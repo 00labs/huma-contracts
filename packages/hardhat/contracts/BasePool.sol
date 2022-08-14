a21e22ff3a850e407b1d5337118ec9d1ba6654ea
wisdant <richard.y.liu@gmail.com>
2022-08-14 11:30:32 -0700

    Revert "remove outdated modifier onlyHumaMasterAdmin"
    This reverts commit 78c6f5ee804007c26eea8e0c5944040f5796ea0f.

--- packages/hardhat/contracts/BasePool.sol
+++ packages/hardhat/contracts/BasePool.sol
@@ -110,6 +110,14 @@ abstract contract BasePool is HDT, ILiquidityProvider, IPool, Ownable {
         emit PoolDeployed(address(this));
     }
 
+    modifier onlyHumaMasterAdmin() {
+        require(
+            msg.sender == HumaConfig(humaConfig).owner(),
+            "BasePool:PERMISSION_DENIED_NOT_MASTER_ADMIN"
+        );
+        _;
+    }
+
     //********************************************/
     //               LP Functions                //
     //********************************************/
