b6db5772c5106519758fe35675d981b6fb394b97
PlayJoker <jpeng.ustc@gmail.com>
2022-08-16 00:16:38 -0700

    prettier optimization

--- packages/hardhat/contracts/InvoiceNFT.sol
+++ packages/hardhat/contracts/InvoiceNFT.sol
@@ -1,6 +1,6 @@
 //Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.4 <0.9.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
