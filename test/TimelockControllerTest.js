e6ab22dfe3a75c59de9a844c8147ef477f52a1e5
bin <bin@57blocks.com>
2022-09-14 20:31:37 +0800

    1. add TimelockController tests 2. move openzeppelin contracts under openzeppelin folder 3. add pool view function of HDT

---
+++ test/TimelockControllerTest.js
@@ -0,0 +1,255 @@
+const {ethers} = require("hardhat");
+const {expect} = require("chai");
+const {deployContracts, deployAndSetupPool} = require("./BaseTest");
+
+describe("TimelockController Test", function () {
+    const salt = ethers.utils.formatBytes32String("salt");
+
+    let poolContract;
+    let hdtContract;
+    let humaConfigContract;
+    let testTokenContract;
+    let feeManagerContract;
+    let defaultDeployer;
+    let proxyOwner;
+    let protocolOwner;
+    let poolOwner;
+    let evaluationAgent;
+    let treasury;
+    let lender;
+    let timelockContract;
+
+    async function advanceClock(seconds) {
+        await ethers.provider.send("evm_increaseTime", [seconds]);
+        await ethers.provider.send("evm_mine");
+    }
+
+    function genOperation(target, value, data, predecessor, salt) {
+        const id = ethers.utils.keccak256(
+            ethers.utils.defaultAbiCoder.encode(
+                ["address", "uint256", "bytes", "uint256", "bytes32"],
+                [target, value, data, predecessor, salt]
+            )
+        );
+        return {id, target, value, data, predecessor, salt};
+    }
+
+    function genOperationBatch(targets, values, payloads, predecessor, salt) {
+        const id = ethers.utils.keccak256(
+            ethers.utils.defaultAbiCoder.encode(
+                ["address[]", "uint256[]", "bytes[]", "uint256", "bytes32"],
+                [targets, values, payloads, predecessor, salt]
+            )
+        );
+        return {id, targets, values, payloads, predecessor, salt};
+    }
+
+    before(async function () {
+        [
+            defaultDeployer,
+            proxyOwner,
+            protocolOwner,
+            poolOwner,
+            evaluationAgent,
+            treasury,
+            lender,
+        ] = await ethers.getSigners();
+    });
+
+    beforeEach(async function () {
+        [humaConfigContract, feeManagerContract, testTokenContract] = await deployContracts(
+            poolOwner,
+            treasury,
+            lender
+        );
+
+        [hdtContract, poolContract] = await deployAndSetupPool(
+            poolOwner,
+            proxyOwner,
+            evaluationAgent,
+            lender,
+            humaConfigContract,
+            feeManagerContract,
+            testTokenContract,
+            0
+        );
+
+        const TimelockController = await ethers.getContractFactory("TimelockController");
+        timelockContract = await TimelockController.deploy(
+            0,
+            [protocolOwner.address],
+            [protocolOwner.address]
+        );
+        await timelockContract.deployed();
+
+        // set timelock as HDT's owner
+        await hdtContract.transferOwnership(timelockContract.address);
+
+        // deployer renounces admin role
+        const adminRole = await timelockContract.TIMELOCK_ADMIN_ROLE();
+        await timelockContract.renounceRole(adminRole, defaultDeployer.address);
+    });
+
+    it("Deployer doesn't have admin role", async function () {
+        const adminRole = await timelockContract.TIMELOCK_ADMIN_ROLE();
+        expect(await timelockContract.hasRole(adminRole, defaultDeployer.address)).equals(false);
+    });
+
+    it("Timelock has admin role", async function () {
+        const adminRole = await timelockContract.TIMELOCK_ADMIN_ROLE();
+        expect(await timelockContract.hasRole(adminRole, timelockContract.address)).equals(true);
+    });
+
+    it("Protocol owner has correct roles", async function () {
+        let role = await timelockContract.PROPOSER_ROLE();
+        expect(await timelockContract.hasRole(role, protocolOwner.address)).equals(true);
+        role = await timelockContract.EXECUTOR_ROLE();
+        expect(await timelockContract.hasRole(role, protocolOwner.address)).equals(true);
+        role = await timelockContract.CANCELLER_ROLE();
+        expect(await timelockContract.hasRole(role, protocolOwner.address)).equals(true);
+    });
+
+    it("Should not allow schedule without correct role", async function () {
+        const data = hdtContract.interface.encodeFunctionData("setPool", [poolOwner.address]);
+        const operation = genOperation(
+            hdtContract.address,
+            0,
+            data,
+            ethers.constants.HashZero,
+            salt
+        );
+        await expect(
+            timelockContract.schedule(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt,
+                0
+            )
+        ).to.be.reverted;
+    });
+
+    it("Should schedule successfully", async function () {
+        const data = hdtContract.interface.encodeFunctionData("setPool", [poolOwner.address]);
+        const operation = genOperation(
+            hdtContract.address,
+            0,
+            data,
+            ethers.constants.HashZero,
+            salt
+        );
+        const delay = 100;
+        await timelockContract
+            .connect(protocolOwner)
+            .schedule(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt,
+                delay
+            );
+        expect(await timelockContract.isOperationPending(operation.id)).equals(true);
+    });
+
+    it("Should cancel successfully", async function () {
+        const data = hdtContract.interface.encodeFunctionData("setPool", [poolOwner.address]);
+        const operation = genOperation(
+            hdtContract.address,
+            0,
+            data,
+            ethers.constants.HashZero,
+            salt
+        );
+        const delay = 100;
+        await timelockContract
+            .connect(protocolOwner)
+            .schedule(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt,
+                delay
+            );
+        expect(await timelockContract.isOperationPending(operation.id)).equals(true);
+
+        // revert with wrong role
+        await expect(timelockContract.cancel(operation.id)).to.be.reverted;
+
+        await timelockContract.connect(protocolOwner).cancel(operation.id);
+        expect(await timelockContract.isOperation(operation.id)).equals(false);
+    });
+
+    it("Should exec successfully", async function () {
+        const data = hdtContract.interface.encodeFunctionData("setPool", [poolOwner.address]);
+        const operation = genOperation(
+            hdtContract.address,
+            0,
+            data,
+            ethers.constants.HashZero,
+            salt
+        );
+        const delay = 100;
+        await timelockContract
+            .connect(protocolOwner)
+            .schedule(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt,
+                delay
+            );
+        expect(await timelockContract.isOperationPending(operation.id)).equals(true);
+
+        // revert with wrong role
+        await expect(
+            timelockContract.execute(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt
+            )
+        ).to.be.reverted;
+
+        // can't execute pending operation
+        await expect(
+            timelockContract
+                .connect(protocolOwner)
+                .execute(
+                    operation.target,
+                    operation.value,
+                    operation.data,
+                    operation.predecessor,
+                    operation.salt
+                )
+        ).to.be.reverted;
+
+        await advanceClock(delay);
+
+        expect(await timelockContract.isOperationReady(operation.id)).equals(true);
+        // can't cancel ready operation
+        await expect(timelockContract.cancel(operation.id)).to.be.reverted;
+
+        expect(await hdtContract.pool()).equals(poolContract.address);
+
+        await timelockContract
+            .connect(protocolOwner)
+            .execute(
+                operation.target,
+                operation.value,
+                operation.data,
+                operation.predecessor,
+                operation.salt
+            );
+
+        expect(await timelockContract.isOperationDone(operation.id)).equals(true);
+        // can't cancel done operation
+        await expect(timelockContract.cancel(operation.id)).to.be.reverted;
+
+        expect(await hdtContract.pool()).equals(poolOwner.address);
+    });
+});
