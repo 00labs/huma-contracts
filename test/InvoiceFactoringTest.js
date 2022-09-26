352a98fe4fe201fa992756e48c4305cf37575f80
Michael Liu <michael.liu95@gmail.com>
2022-09-26 11:53:51 -0700

    Update InvoiceFactoringTest.js

--- test/InvoiceFactoringTest.js
+++ test/InvoiceFactoringTest.js
@@ -548,7 +548,7 @@ describe("Invoice Factoring", function () {
             ).to.be.revertedWith("paymentDetectionServiceAccountRequired()");
         });
 
-        it.only("Process payback", async function () {
+        it("Process payback", async function () {
             expect(await testTokenContract.balanceOf(borrower.address)).to.equal(386);
             await ethers.provider.send("evm_increaseTime", [30 * 24 * 3600 - 10]);
 
@@ -560,7 +560,7 @@ describe("Invoice Factoring", function () {
                 .onReceivedPayment(borrower.address, testTokenContract.address, 500, 1);
             await expect(
                 invoiceContract
-                    .connect(evaluationAgent)
+                    .connect(pdsServiceAccount)
                     .onReceivedPayment(borrower.address, testTokenContract.address, 500, 1)
             ).to.be.revertedWith("HumaIF:ALREADY_PROCESSED");
 
