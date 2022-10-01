3b9d9ba4f2c9112a102c5c5627038b8891f25206
Richard Liu <wisdant@users.noreply.github.com>
2022-10-01 12:27:55 -0700

    Change license to AGPL-3.0-or-later (#173)
    * Change license to AGPL-3.0-or-later
    
    * change license for HDT

--- contracts/interfaces/ILiquidityProvider.sol
+++ contracts/interfaces/ILiquidityProvider.sol
@@ -1,6 +1,5 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
-
 
 interface ILiquidityProvider {
     function deposit(uint256 amount) external;
