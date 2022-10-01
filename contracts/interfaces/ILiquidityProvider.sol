bd8b66d858face8c0ccc0f9a25a0dab366cd4abb
wisdant <richard.y.liu@gmail.com>
2022-10-01 11:08:30 -0700

    Revert "Change license to AGPL-3.0-or-later"
    This reverts commit 028643c04e7d1cccb64dddf183b450654316723e.

--- contracts/interfaces/ILiquidityProvider.sol
+++ contracts/interfaces/ILiquidityProvider.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 
 interface ILiquidityProvider {
     function deposit(uint256 amount) external;
