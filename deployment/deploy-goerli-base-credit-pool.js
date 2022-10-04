231e7c1c0e2f4b7276bdbb3f7aed2e60abe870e5
PlayJoker <jpeng.ustc@gmail.com>
2022-10-04 13:14:38 -0700

    separted receivableFactoringPool and baseCreditPool deployment

--- deployment/deploy-goerli-base-credit-pool.js
+++ deployment/deploy-goerli-base-credit-pool.js
@@ -14,41 +14,12 @@ async function deployContracts() {
     const eaService = await accounts[4];
     console.log("ea service address: " + eaService.address);
 
-    // Deploying Receivable factoring pool
     const usdc = await deploy("TestToken", "USDC");
+    const evaluation_agent_NFT = await deploy("EvaluationAgentNFT", "EANFT", [], eaService);
+    const invoice_NFT = await deploy("InvoiceNFT", "RNNFT", [usdc.address]);
 
     const humaConfig = await deploy("HumaConfig", "HumaConfig");
     const humaConfigTL = await deploy("TimelockController", "HumaConfigTimelock", [
-        0,
-        [deployer.address],
-        [deployer.address],
-    ]);
-
-    const feeManager = await deploy("BaseFeeManager", "ReceivableFactoringPoolFeeManager");
-    const hdtImpl = await deploy("HDT", "HDTImpl");
-    const hdt = await deploy("TransparentUpgradeableProxy", "HDT", [
-        hdtImpl.address,
-        proxyOwner.address,
-        [],
-    ]);
-
-    const poolConfig = await deploy("BasePoolConfig", "ReceivableFactoringPoolConfig");
-
-    const poolImpl = await deploy("ReceivableFactoringPool", "ReceivableFactoringPoolImpl");
-    const pool = await deploy("TransparentUpgradeableProxy", "ReceivableFactoringPool", [
-        poolImpl.address,
-        proxyOwner.address,
-        [],
-    ]);
-
-    const evaluation_agent_NFT = await deploy("EvaluationAgentNFT", "EANFT", [], eaService);
-
-    const invoice_NFT = await deploy("InvoiceNFT", "RNNFT", [usdc.address]);
-    // End of deploying receivable factoring pool
-
-    // Deploying base credit pool
-    const bc_humaConfig = await deploy("HumaConfig", "BaseCreditPoolHumaConfig");
-    const bc_humaConfigTLBC = await deploy("TimelockController", "BaseCreditPoolHumaConfigTimelock", [
         0,
         [deployer.address],
         [deployer.address],
