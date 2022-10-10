e239ccb481edb6395f573a21c50536958fba97a9
bin-57blocks <52771832+bin-57blocks@users.noreply.github.com>
2022-10-10 18:50:59 +0800

    List all NFT Ids of a given user (#222)
    add user's NFT list

---
+++ test/InvoiceNFTTest.js
@@ -0,0 +1,73 @@
+const {ethers} = require("hardhat");
+const {expect} = require("chai");
+
+describe("InvoiceNFT Test", function () {
+    let deployer, user1, user2, user3;
+
+    let testToken, nft;
+
+    before(async function () {
+        [deployer, user1, user2, user3] = await ethers.getSigners();
+    });
+
+    beforeEach(async function () {
+        const TestToken = await ethers.getContractFactory("TestToken");
+        testToken = await TestToken.deploy();
+        await testToken.deployed();
+
+        const InvoiceNFT = await ethers.getContractFactory("InvoiceNFT");
+        nft = await InvoiceNFT.deploy(testToken.address);
+        await nft.deployed();
+    });
+
+    async function verifyNFTs(user, nftIds) {
+        const ids = await nft.getNFTIds(user.address);
+        expect(ids.toString()).equals(nftIds.toString());
+    }
+
+    async function verifyNFTIndexes(indexes) {
+        const res = [];
+        for (let i = 1; i <= 5; i++) {
+            res[i - 1] = await nft.getTokenIdIndex(i);
+        }
+        expect(res.toString()).equals(indexes.toString());
+    }
+
+    it("list NFTs", async function () {
+        await nft.mintNFT(user1.address, "test1");
+        await nft.mintNFT(user1.address, "test2");
+        await nft.mintNFT(user1.address, "test3");
+        await verifyNFTs(user1, [1, 2, 3]);
+
+        await nft.mintNFT(user2.address, "test4");
+        await nft.mintNFT(user2.address, "test5");
+        await verifyNFTs(user2, [4, 5]);
+
+        await nft.connect(user1).transferFrom(user1.address, user2.address, 1);
+        await verifyNFTs(user1, [3, 2]);
+        await verifyNFTs(user2, [4, 5, 1]);
+        await verifyNFTIndexes([3, 2, 1, 1, 2]);
+
+        await nft
+            .connect(user2)
+            ["safeTransferFrom(address,address,uint256)"](user2.address, user1.address, 5);
+        await verifyNFTs(user1, [3, 2, 5]);
+        await verifyNFTs(user2, [4, 1]);
+        await verifyNFTIndexes([2, 2, 1, 1, 3]);
+
+        await nft.connect(user1).approve(user3.address, 3);
+        await nft.connect(user1).approve(user3.address, 2);
+        await nft.connect(user1).approve(user3.address, 5);
+        await nft.connect(user2).approve(user3.address, 4);
+        await verifyNFTIndexes([2, 2, 1, 1, 3]);
+
+        await nft.connect(user3).transferFrom(user1.address, user3.address, 5);
+        await nft.connect(user3).transferFrom(user1.address, user3.address, 2);
+        await nft.connect(user3).transferFrom(user1.address, user3.address, 3);
+        await nft.connect(user3).transferFrom(user2.address, user3.address, 4);
+        await verifyNFTs(user1, []);
+        await verifyNFTs(user2, [1]);
+        await verifyNFTs(user3, [5, 2, 3, 4]);
+        await verifyNFTIndexes([1, 2, 3, 4, 1]);
+    });
+});
